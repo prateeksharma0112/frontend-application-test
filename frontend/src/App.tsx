aa292b6c2322f3af259ad70beef851a329d331b1
prateek.sharma <prateeksharma0112@gmail.com>
2025-02-27 17:12:10 +0100

    add newpage route and configure routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router";
 
 import Layout from "@/app/dashboard/page";
 import Testpage from "@/app/testpage/page";
+import Newpage from "@/app/newpage/page";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Layout />}>
                 <Route index element={<Testpage />} />
+                <Route path="/newpage" element={<Newpage />} />
             </Route>
         </Routes>
     );
